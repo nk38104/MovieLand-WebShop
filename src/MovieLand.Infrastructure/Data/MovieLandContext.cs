c1ac8d52126937f894a0a67c5d1c5dafcb507a4a
Nikola Kelava <nikola.kelava94@gmail.com>
2021-08-16 00:18:39 +0200

    Add connection to database

---
+++ src/MovieLand.Infrastructure/Data/MovieLandContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace MovieLand.Infrastructure.Data
+{
+    public class MovieLandContext : IdentityDbContext<IdentityUser>
+    {
+        public MovieLandContext(DbContextOptions options) : base(options)
+        {}
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- src/MovieLand.Web/Program.cs
+++ src/MovieLand.Web/Program.cs
@@ -1,11 +1,11 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MovieLand.Infrastructure.Data;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace MovieLand.Web
 {
@@ -13,14 +13,37 @@ namespace MovieLand.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            SeedDatabase(host);
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+
+        private static void SeedDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    var movieLandContext = services.GetRequiredService<MovieLandContext>();
+                    //MovieLandContextSeed.SeedAsync(movieLandContext, loggerFactory).Wait();
+                }
+                catch (Exception exception)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(exception, "An error occurred seeding the DB.");
+                }
+            }
+        }
     }
 }
