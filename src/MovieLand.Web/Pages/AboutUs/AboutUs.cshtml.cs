78ec33d22b07aa0990faf4beff4baba77ac84a91
Nikola Kelava <nikola.kelava94@gmail.com>
2021-08-23 12:53:57 +0200

    Add simple template for AboutUs and ContactUs

---
+++ src/MovieLand.Web/Pages/AboutUs/AboutUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace MovieLand.Web.Pages
+{
+    public class AboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
