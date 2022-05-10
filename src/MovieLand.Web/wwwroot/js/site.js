081756ef021d5ccbf6c2e1611cc0c3317ec1f5b8
Nikola Kelava <nikola.kelava94@gmail.com>
2022-05-10 20:12:26 +0200

    Delete all pages html and css

--- src/MovieLand.Web/wwwroot/js/site.js
+++ src/MovieLand.Web/wwwroot/js/site.js
@@ -2,8 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-// Material Select Initialization
-$(document).ready(function () {
-    $('.mdb-select').materialSelect();
-});
